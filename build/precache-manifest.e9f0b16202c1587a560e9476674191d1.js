2f3b88dc59954d363fe8af439f0415001c2716ec
niwek <kewinli2@gmail.com>
2018-10-27 14:12:15 -0400

    Reinstalled reactjs app

---
+++ build/precache-manifest.e9f0b16202c1587a560e9476674191d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e30236e79132a11c2947",
+    "url": "/static/js/main.e30236e7.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e30236e79132a11c2947",
+    "url": "/static/css/main.003c2099.chunk.css"
+  },
+  {
+    "revision": "3b835dbcc5611207e41500175531e666",
+    "url": "/index.html"
+  }
+];
