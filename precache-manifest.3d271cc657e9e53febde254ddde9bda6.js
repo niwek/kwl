0df3fcbcfda41b2e4844113e53c30d4d8563ebb9
niwek <kewinli2@gmail.com>
2018-10-27 23:33:30 -0400

    Updates

---
+++ precache-manifest.3d271cc657e9e53febde254ddde9bda6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kwl/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81046aa84af266880925",
+    "url": "/kwl/static/js/runtime~main.81046aa8.js"
+  },
+  {
+    "revision": "70873228300d878f40c7",
+    "url": "/kwl/static/js/main.70873228.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/kwl/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "70873228300d878f40c7",
+    "url": "/kwl/static/css/main.d61101b0.chunk.css"
+  },
+  {
+    "revision": "8a6281fa8686acbdcb72cb72d5c784ec",
+    "url": "/kwl/index.html"
+  }
+];
